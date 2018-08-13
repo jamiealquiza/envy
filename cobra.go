2c7680e4c27d65d4212675d6738727860fffc5b7
Kamal Nasser <hello@kamal.io>
2018-08-14 00:22:34 +0300

    Replace dots with underscores in flag names

--- cobra.go
+++ cobra.go
@@ -69,6 +69,7 @@ func updateCobra(p string, fs *pflag.FlagSet) {
 		// based on the supplied prefix.
 		envVar := fmt.Sprintf("%s_%s", p, strings.ToUpper(f.Name))
 		envVar = strings.Replace(envVar, "-", "_", -1)
+		envVar = strings.Replace(envVar, ".", "_", -1)
 
 		// Update the Flag.Value if the
 		// env var is non "".
